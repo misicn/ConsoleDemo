3b6b7a21212ecbb20cf5b3bc36608fde1d0e2a6e
nikola <misicn@gmail.com>
2024-10-28 23:03:17 +0000

    adding another test

--- src/test/java/org/misicx/tests/GreeterTests.java
+++ src/test/java/org/misicx/tests/GreeterTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.misicx.Greeter;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GreeterTests {
@@ -11,6 +12,10 @@ public class GreeterTests {
     @Test
     public void greetsCorrectly() {
         assertThat(Greeter.greet(), equalTo("Hello World!"));
+    }
 
+    @Test
+    public void doesNotGreetIncorrectly() {
+        assertThat(Greeter.greet(), not(equalTo("wrong!")));
     }
 }
